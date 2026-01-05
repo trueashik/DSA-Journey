2710cf0e325bca7470be889d4c1134bb46ec4c5e
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2026-01-05 17:03:13 +0530

    File updated at 1/5/2026, 5:03:13 PM

--- 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.cpp
+++ 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.cpp
@@ -1,39 +1,40 @@
-// Last updated: 1/5/2026, 5:02:08 PM
+// Last updated: 1/5/2026, 5:03:13 PM
 1class Solution {
-2public:
-3
-4    void backtrack(string& digits,vector<string>& mp, vector<string>& res, string& draft, int idx){
-5        if(idx == digits.length()){
-6            res.push_back(draft);
-7            return;
-8        }
-9        int num = digits[idx] - '0';
-10        for(int i = 0; i < mp[num].length(); i++){
-11            draft += mp[num][i];
-12            backtrack(digits, mp, res, draft, idx + 1);
-13            draft.pop_back();
-14        }
-15    }
-16    vector<string> letterCombinations(string digits) {
-17        if(digits.empty()) return {};
-18        vector<string> db = {
-19            "",
+2     // Here i am using backtracking with TC O(4^n) and SC O(n);
+3public:
+4
+5    void backtrack(string& digits,vector<string>& mp, vector<string>& res, string& draft, int idx){
+6        if(idx == digits.length()){
+7            res.push_back(draft);
+8            return;
+9        }
+10        int num = digits[idx] - '0';
+11        for(int i = 0; i < mp[num].length(); i++){
+12            draft += mp[num][i];
+13            backtrack(digits, mp, res, draft, idx + 1);
+14            draft.pop_back();
+15        }
+16    }
+17    vector<string> letterCombinations(string digits) {
+18        if(digits.empty()) return {};
+19        vector<string> db = {
 20            "",
-21            "abc",
-22            "def",
-23            "ghi",
-24            "jkl",
-25            "mno",
-26            "pqrs",
-27            "tuv",
-28            "wxyz"
-29        };
-30        string draft;
-31        vector<string> res;
-32        backtrack(digits, db, res, draft, 0);
-33
-34        return res;
-35
-36        
-37    }
-38};
+21            "",
+22            "abc",
+23            "def",
+24            "ghi",
+25            "jkl",
+26            "mno",
+27            "pqrs",
+28            "tuv",
+29            "wxyz"
+30        };
+31        string draft;
+32        vector<string> res;
+33        backtrack(digits, db, res, draft, 0);
+34
+35        return res;
+36
+37        
+38    }
+39};
