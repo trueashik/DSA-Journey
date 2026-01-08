945d4df9f2dd6e5f14fde6d48040ba4e55a1f52a
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2026-01-08 22:05:56 +0530

    File updated at 1/8/2026, 10:05:54 PM

--- 208-ImplementTrie(PrefixTree)/208-ImplementTrie(PrefixTree).cpp
+++ 208-ImplementTrie(PrefixTree)/208-ImplementTrie(PrefixTree).cpp
@@ -1,64 +1,65 @@
-// Last updated: 1/8/2026, 10:05:09 PM
+// Last updated: 1/8/2026, 10:05:54 PM
 1class Trie {
-2public:
-3    struct Node {
-4        Node* next[26] = {};
-5        bool isend = false;
-6    };
-7    
-8    Node* root;
-9    Trie() {
-10        root = new Node();
-11        
-12    }
-13    
-14    void insert(string word) {  
-15        Node* cur = root;    
-16        for(int i = 0; i < word.length(); i++){
-17            int idx = word[i] - 'a';
-18
-19            if(!cur->next[idx]){
-20                cur->next[idx] = new Node();
-21            }
-22            cur = cur->next[idx];
-23
-24        }
-25        cur->isend = true;
-26        
-27    }
-28    
-29    bool search(string word) {
-30        Node* cur = root;    
-31        for(int i = 0; i < word.length(); i++){
-32            int idx = word[i] - 'a';
-33
-34            if(cur->next[idx] == nullptr) return false;
-35            cur = cur->next[idx];
-36
-37        }
-38        if(cur->isend) return true;
-39        return false;
-40        
-41    }
-42    
-43    bool startsWith(string prefix) {
-44        Node* cur = root;    
-45        for(int i = 0; i < prefix.length(); i++){
-46            int idx = prefix[i] - 'a';
-47
-48            if(cur->next[idx] == nullptr) return false;
-49            cur = cur->next[idx];
-50
-51        }
-52        return true;
-53        
-54    }
-55};
-56
-57/**
-58 * Your Trie object will be instantiated and called as such:
-59 * Trie* obj = new Trie();
-60 * obj->insert(word);
-61 * bool param_2 = obj->search(word);
-62 * bool param_3 = obj->startsWith(prefix);
-63 */
+2    // Here used trie with tc & SC O(L);
+3public:
+4    struct Node {
+5        Node* next[26] = {};
+6        bool isend = false;
+7    };
+8    
+9    Node* root;
+10    Trie() {
+11        root = new Node();
+12        
+13    }
+14    
+15    void insert(string word) {  
+16        Node* cur = root;    
+17        for(int i = 0; i < word.length(); i++){
+18            int idx = word[i] - 'a';
+19
+20            if(!cur->next[idx]){
+21                cur->next[idx] = new Node();
+22            }
+23            cur = cur->next[idx];
+24
+25        }
+26        cur->isend = true;
+27        
+28    }
+29    
+30    bool search(string word) {
+31        Node* cur = root;    
+32        for(int i = 0; i < word.length(); i++){
+33            int idx = word[i] - 'a';
+34
+35            if(cur->next[idx] == nullptr) return false;
+36            cur = cur->next[idx];
+37
+38        }
+39        if(cur->isend) return true;
+40        return false;
+41        
+42    }
+43    
+44    bool startsWith(string prefix) {
+45        Node* cur = root;    
+46        for(int i = 0; i < prefix.length(); i++){
+47            int idx = prefix[i] - 'a';
+48
+49            if(cur->next[idx] == nullptr) return false;
+50            cur = cur->next[idx];
+51
+52        }
+53        return true;
+54        
+55    }
+56};
+57
+58/**
+59 * Your Trie object will be instantiated and called as such:
+60 * Trie* obj = new Trie();
+61 * obj->insert(word);
+62 * bool param_2 = obj->search(word);
+63 * bool param_3 = obj->startsWith(prefix);
+64 */
