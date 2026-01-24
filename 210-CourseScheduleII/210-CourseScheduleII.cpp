05859587d085f10327cfa3c5d4ec5bfbc3da5bd3
Ashik Ahmed <154865503+ashikahmed05@users.noreply.github.com>
2026-01-24 13:34:54 +0530

    File updated at 1/24/2026, 1:34:54 PM

--- 210-CourseScheduleII/210-CourseScheduleII.cpp
+++ 210-CourseScheduleII/210-CourseScheduleII.cpp
@@ -1,31 +1,32 @@
-// Last updated: 1/24/2026, 1:29:42 PM
+// Last updated: 1/24/2026, 1:34:54 PM
 1class Solution {
-2public:
-3    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-4        vector<vector<int>> nums(numCourses);
-5        vector<int> indegree(numCourses, 0);
-6        vector<int>res;
-7        
-8        for(int i = 0; i < prerequisites.size(); i++){
-9            indegree[prerequisites[i][0]]++;
-10            nums[prerequisites[i][1]].push_back(prerequisites[i][0]);
-11        }
-12        queue<int>q;
-13        for(int i = 0; i < indegree.size(); i++){
-14            if(indegree[i]==0)q.push(i);
-15        }
-16        while(!q.empty()){
-17            for(int i : nums[q.front()]){
-18                indegree[i]--;
-19                if(indegree[i] == 0) q.push(i);
-20            }
-21            res.push_back(q.front());
-22            q.pop();
-23
-24        }
-25        if(res.size() == numCourses){
-26            return res;
-27        }
-28        return {};
-29    }
-30};
+2    // Here i have done it with Kahn's Algorithm with sc and tc as O(V+E)
+3public:
+4    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+5        vector<vector<int>> nums(numCourses);
+6        vector<int> indegree(numCourses, 0);
+7        vector<int>res;
+8        
+9        for(int i = 0; i < prerequisites.size(); i++){
+10            indegree[prerequisites[i][0]]++;
+11            nums[prerequisites[i][1]].push_back(prerequisites[i][0]);
+12        }
+13        queue<int>q;
+14        for(int i = 0; i < indegree.size(); i++){
+15            if(indegree[i]==0)q.push(i);
+16        }
+17        while(!q.empty()){
+18            for(int i : nums[q.front()]){
+19                indegree[i]--;
+20                if(indegree[i] == 0) q.push(i);
+21            }
+22            res.push_back(q.front());
+23            q.pop();
+24
+25        }
+26        if(res.size() == numCourses){
+27            return res;
+28        }
+29        return {};
+30    }
+31};
